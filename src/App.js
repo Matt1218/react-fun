253f6f37eb06f7258a93747929329db6e4d4021d
Matt1218 <welfleym@gmail.com>
2022-06-04 21:52:18 -0400

    components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import Backdrop from "./Components/Backdrop";
+import Modal from "./Components/Modal";
 import Todo from "./Components/Todo";
 
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo text="1"/>
-      <Todo text="2"/>
-      <Todo text="3"/>
+      <Todo text="1" />
+      <Todo text="2" />
+      <Todo text="3" />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,9 +1,15 @@
 function Todo(props) {
+  function deleteHandler() {
+      
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        {/* Do not add paranthesis to deleteHandler with onClick or else it will
+        execute on loading of JS */}
+        <button className="btn" onClick={deleteHandler}>Delete</button>
       </div>
     </div>
   );
